f2095f7d3a76386140b4b7bfc8db653f34fe6849
hyungul.roh <knowhunger@gmail.com>
2021-07-28 05:20:56 +0900

    🛠chores: update git tag when npm publish

--- postpublish.sh
+++
@@ -1,6 +0,0 @@
-#!bash
-
-PACKAGE_VERSION=$(cat package.json | grep "version" | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-echo $PACKAGE_VERSION
-git tag v$PACKAGE_VERSION -m "https://www.npmjs.com/package/@orda-io/orda-integer/v/$PACKAGE_VERSION"
-git push --tags
