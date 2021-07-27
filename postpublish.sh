eb4ebeb4f1798603d275cf97a820545dc49a23a9
hyungul.roh <knowhunger@gmail.com>
2021-07-28 05:04:49 +0900

    🛠chores: add git tag when npm publish

---
+++ postpublish.sh
@@ -0,0 +1,6 @@
+#!bash
+
+PACKAGE_VERSION=$(cat package.json | grep "version" | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+echo $PACKAGE_VERSION
+git tag v$PACKAGE_VERSION
+git push --tags
